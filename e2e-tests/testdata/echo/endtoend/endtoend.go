encoredev/encore

Add encore.dev/types/option package
This adds a new `encore.dev/types/option` package for Encore.go,
with a new type option.Option[T] for representing optional values.

This improves Encore's support for representing optional types in APIs.
It's also something that we've internally found to be useful again and again
in large code bases, where it's difficult to keep track of whether a pointer value
*T means the type is nullable or not.

e2e-tests/testdata/echo/endtoend/endtoend.go
@@ -5,16 +5,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
+
 	// "net/http"
 	// "net/http/httptest"
 	"os"
 	"reflect"
+
 	// "strings"
 	"time"
 
 	"encore.app/test"
 	"encore.dev/beta/errs"
 	"encore.dev/rlog"
+	"encore.dev/types/option"
 	"encore.dev/types/uuid"
 )
 
@@ -92,35 +95,38 @@ func GeneratedWrappersEndToEndTest(ctx context.Context) (err error) {
 	r.Read(queryBytes)
 	r.Read(bodyBytes)
 	params := &test.MarshallerTest[int]{
-		HeaderBoolean: r.Float32() > 0.5,
-		HeaderInt:     r.Int(),
-		HeaderFloat:   r.Float64(),
-		HeaderString:  "header string",
-		HeaderBytes:   headerBytes,
-		HeaderTime:    time.Now().Truncate(time.Second),
-		HeaderJson:    json.RawMessage("{\"hello\":\"world\"}"),
-		HeaderUUID:    newUUID(),
-		HeaderUserID:  "432",
-		QueryBoolean:  r.Float32() > 0.5,
-		QueryInt:      r.Int(),
-		QueryFloat:    r.Float64(),
-		QueryString:   "query string",
-		QueryBytes:    headerBytes,
-		QueryTime:     time.Now().Add(time.Duration(rand.Intn(1024)) * time.Hour).Truncate(time.Second),
-		QueryJson:     json.RawMessage("true"),
-		QueryUUID:     newUUID(),
-		QueryUserID:   "9udfa",
-		QuerySlice:    []int{r.Int(), r.Int(), r.Int(), r.Int()},
-		BodyBoolean:   r.Float32() > 0.5,
-		BodyInt:       r.Int(),
-		BodyFloat:     r.Float64(),
-		BodyString:    "body string",
-		BodyBytes:     bodyBytes,
-		BodyTime:      time.Now().Add(time.Duration(rand.Intn(1024)) * time.Hour).Truncate(time.Second),
-		BodyJson:      json.RawMessage("null"),
-		BodyUUID:      newUUID(),
-		BodyUserID:    "✉️",
-		BodySlice:     []int{r.Int(), r.Int(), r.Int(), r.Int(), r.Int(), r.Int()},
+		HeaderBoolean:     r.Float32() > 0.5,
+		HeaderInt:         r.Int(),
+		HeaderFloat:       r.Float64(),
+		HeaderString:      "header string",
+		HeaderBytes:       headerBytes,
+		HeaderTime:        time.Now().Truncate(time.Second),
+		HeaderJson:        json.RawMessage("{\"hello\":\"world\"}"),
+		HeaderUUID:        newUUID(),
+		HeaderUserID:      "432",
+		HeaderOption:      option.Some("test"),
+		QueryBoolean:      r.Float32() > 0.5,
+		QueryInt:          r.Int(),
+		QueryFloat:        r.Float64(),
+		QueryString:       "query string",
+		QueryBytes:        headerBytes,
+		QueryTime:         time.Now().Add(time.Duration(rand.Intn(1024)) * time.Hour).Truncate(time.Second),
+		QueryJson:         json.RawMessage("true"),
+		QueryUUID:         newUUID(),
+		QueryUserID:       "9udfa",
+		QuerySlice:        []int{r.Int(), r.Int(), r.Int(), r.Int()},
+		QuerySliceOptions: []option.Option[int]{option.Some(r.Int()), option.None[int](), option.Some(r.Int())},
+		BodyBoolean:       r.Float32() > 0.5,
+		BodyInt:           r.Int(),
+		BodyFloat:         r.Float64(),
+		BodyString:        "body string",
+		BodyBytes:         bodyBytes,
+		BodyTime:          time.Now().Add(time.Duration(rand.Intn(1024)) * time.Hour).Truncate(time.Second),
+		BodyJson:          json.RawMessage("null"),
+		BodyUUID:          newUUID(),
+		BodyUserID:        "✉️",
+		BodySlice:         []int{r.Int(), r.Int(), r.Int(), r.Int(), r.Int(), r.Int()},
+		BodyOption:        option.Some(r.Int()),
 	}
 	mResp, err := test.MarshallerTestHandler(ctx, params)
 	assert(err, nil, "Expected no error from the marshaller test")

Olimi-org/Afterpiece

e2e-tests/testdata/echo/endtoend/endtoend.go
@@ -5,16 +5,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
+
 	// "net/http"
 	// "net/http/httptest"
 	"os"
 	"reflect"
+
 	// "strings"
 	"time"
 
 	"encore.app/test"
 	"encore.dev/beta/errs"
 	"encore.dev/rlog"
+	"encore.dev/types/option"
 	"encore.dev/types/uuid"
 )
 
@@ -92,35 +95,38 @@ func GeneratedWrappersEndToEndTest(ctx context.Context) (err error) {
 	r.Read(queryBytes)
 	r.Read(bodyBytes)
 	params := &test.MarshallerTest[int]{
-		HeaderBoolean: r.Float32() > 0.5,
-		HeaderInt:     r.Int(),
-		HeaderFloat:   r.Float64(),
-		HeaderString:  "header string",
-		HeaderBytes:   headerBytes,
-		HeaderTime:    time.Now().Truncate(time.Second),
-		HeaderJson:    json.RawMessage("{\"hello\":\"world\"}"),
-		HeaderUUID:    newUUID(),
-		HeaderUserID:  "432",
-		QueryBoolean:  r.Float32() > 0.5,
-		QueryInt:      r.Int(),
-		QueryFloat:    r.Float64(),
-		QueryString:   "query string",
-		QueryBytes:    headerBytes,
-		QueryTime:     time.Now().Add(time.Duration(rand.Intn(1024)) * time.Hour).Truncate(time.Second),
-		QueryJson:     json.RawMessage("true"),
-		QueryUUID:     newUUID(),
-		QueryUserID:   "9udfa",
-		QuerySlice:    []int{r.Int(), r.Int(), r.Int(), r.Int()},
-		BodyBoolean:   r.Float32() > 0.5,
-		BodyInt:       r.Int(),
-		BodyFloat:     r.Float64(),
-		BodyString:    "body string",
-		BodyBytes:     bodyBytes,
-		BodyTime:      time.Now().Add(time.Duration(rand.Intn(1024)) * time.Hour).Truncate(time.Second),
-		BodyJson:      json.RawMessage("null"),
-		BodyUUID:      newUUID(),
-		BodyUserID:    "✉️",
-		BodySlice:     []int{r.Int(), r.Int(), r.Int(), r.Int(), r.Int(), r.Int()},
+		HeaderBoolean:   r.Float32() > 0.5,
+		HeaderInt:       r.Int(),
+		HeaderFloat:     r.Float64(),
+		HeaderString:    "header string",
+		HeaderBytes:     headerBytes,
+		HeaderTime:      time.Now().Truncate(time.Second),
+		HeaderJson:      json.RawMessage("{\"hello\":\"world\"}"),
+		HeaderUUID:      newUUID(),
+		HeaderUserID:    "432",
+		HeaderOption:    option.Some("test"),
+		QueryBoolean:    r.Float32() > 0.5,
+		QueryInt:        r.Int(),
+		QueryFloat:      r.Float64(),
+		QueryString:     "query string",
+		QueryBytes:      headerBytes,
+		QueryTime:       time.Now().Add(time.Duration(rand.Intn(1024)) * time.Hour).Truncate(time.Second),
+		QueryJson:       json.RawMessage("true"),
+		QueryUUID:       newUUID(),
+		QueryUserID:     "9udfa",
+		QuerySlice:      []int{r.Int(), r.Int(), r.Int(), r.Int()},
+		BodyBoolean:     r.Float32() > 0.5,
+		BodyInt:         r.Int(),
+		BodyFloat:       r.Float64(),
+		BodyString:      "body string",
+		BodyBytes:       bodyBytes,
+		BodyTime:        time.Now().Add(time.Duration(rand.Intn(1024)) * time.Hour).Truncate(time.Second),
+		BodyJson:        json.RawMessage("null"),
+		BodyUUID:        newUUID(),
+		BodyUserID:      "✉️",
+		BodySlice:       []int{r.Int(), r.Int(), r.Int(), r.Int(), r.Int(), r.Int()},
+		BodyOption:      option.Some(r.Int()),
+		BodyOptionSlice: []option.Option[int]{option.Some(r.Int()), option.None[int](), option.Some(r.Int())},
 	}
 	mResp, err := test.MarshallerTestHandler(ctx, params)
 	assert(err, nil, "Expected no error from the marshaller test")